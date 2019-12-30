d5711856ba89aaf9af2b4b97f03f5bfdb410fd65
1670691647@qq.com <167091647@qq.com>
2019-12-30 18:14:13 +0000

    Contains a time bomb program, but it is not successed

--- Mychessgame/src/chessgame/chess.java
+++ Mychessgame/src/chessgame/chess.java
@@ -8,24 +8,31 @@ public class chess {
 			char[][] Chessboard=new char[6][7];
 			char[] chesspiece= {'X','O'};
 			int[] timesOfBlitz= {1,1};//every player can play once
+			int[] timesOfTimeBomb={1,1};
+			int totalChess=0;//The number of all pieces that have been placed
+			//The number of rounds,even numbers represent player 1 and odd numbers represent player 2;
+			int rounds=0;
+			// It clears the counters after 3 rounds :
+			//current player makes one move,and next player makes two moves. 
+			int[] roundsOfTimeBomb={-1,-1};
+			int column=-1;
+			int placerow=-1;//The row of current chess piece
+			int modeAscii=-1;
+			int rowOfTimeBomb=-1, columnOfTimeBomb=-1;
+			boolean ifCanPlace=false;
 			//Initialize chess board elements
 			Initchessboard(Chessboard,ChessboardLine);
 			//Draw chess board
-			DressBoard(Chessboard);
-			int totalChess=0;//The number of all pieces that have been placed
-	//The number of rounds,even numbers represent player 1 and odd numbers represent player 2;
-			int rounds=0;
-			int column=-1;
-			int placerow=-1;
+			DressBoard(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
 			while(totalChess<6*7)
 			{  
 				//choose chess playing mode:B,T or 1-7
-				int modeAscii=InputMode(rounds,Chessboard,ChessboardLine);
+			modeAscii=InputMode(rounds,Chessboard,ChessboardLine);
 			    if(modeAscii==66)//B
 			    {  
 			    	if(timesOfBlitz[rounds%2]==1)
 			    	{
-			    	//Call instantaneous bomb function
+			    		
 			    	System.out.printf("Blitz please select column >");
 			        column=Integer.parseInt(stdin.nextLine())-1;
 			    	for( int row=0;row<6;row++)
@@ -36,7 +43,7 @@ public class chess {
 			    			totalChess--;	
 			    		}		
 			    	} 
-			    	DressBoard(Chessboard);
+			    	DressBoard(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
 			    	timesOfBlitz[rounds%2]--;
 			    	rounds++;
 			    	}
@@ -47,83 +54,61 @@ public class chess {
 			    }
 			    else if(modeAscii==84)//T
 			    {
-			    		
+			    		if(timesOfTimeBomb[rounds%2]==1)
+			    		{  
+			    			roundsOfTimeBomb[rounds%2]=2;
+			    			System.out.printf("Time bomb please select column >");
+					        column=Integer.parseInt(stdin.nextLine())-1;
+					    	for( int row=0;row<6;row++)
+					    	{
+					    		if(Chessboard[row][column]==ChessboardLine)
+					    		{   
+					    			Chessboard[row][column]='*';
+					    			rowOfTimeBomb=row;
+					    			columnOfTimeBomb=column;
+					    			break;	
+					    		}		
+					    	} 
+					    			    	
+					    	DressBoard(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
+					    	totalChess++;
+					    	timesOfBlitz[rounds%2]--;
+					    	roundsOfTimeBomb[rounds%2]=rounds+3;
+					    	rounds++;	
+			    }
 			    }
 			    else if(modeAscii>=49&&modeAscii<56)//1-7
 			    {
 			    column=modeAscii-49;//Actual column
 			  //Place chess piece
-			    boolean ifCanPlace=IfCanPlacePiece(column,Chessboard,ChessboardLine);
+			    ifCanPlace=IfCanPlacePiece(column,Chessboard,ChessboardLine);
 			    if(ifCanPlace)
 			    {
-			      placerow=Place(column,rounds,Chessboard,ChessboardLine,chesspiece);
+			      placerow=Place(totalChess,column,rounds,Chessboard,ChessboardLine,chesspiece,roundsOfTimeBomb,rowOfTimeBomb,columnOfTimeBomb);
 			      totalChess++;
 				rounds++;
 			    }
 				//Judge if anyone wins
 				if(judgeWinner(Chessboard,Chessboard[placerow][column],placerow,column))
 				{
-					System.out.printf("Congratulations,play %d wins",rounds%2+1);
+					System.out.printf("Congratulations,play %d wins",rounds%2);
 					break;
 				}
-			    }  
+			    } 
+			 
 	            if(totalChess==42)
 	            {
 	            	System.out.printf("The chessboard is full. This game is a draw\n");
 	            	break;
 	            }
-				}		    
-			  
-			    }
-			
-	    public static int Place(int column,int rounds,char[][] Chessboard,char ChessboardLine,char[] chesspiece)
-	    {
-	    	int Placerow=0;
-		//If ifCanPlace is true,it means It means that pieces can be placed
-		//Judge which grid to put chess on. 
-			for(int row=0;row<6;row++)//row
-			{
-			  if(Chessboard[row][column]==ChessboardLine)
-				{
-					Chessboard[row][column]=chesspiece[rounds%2];
-					Placerow=row;
-					break;
-				}
-					continue;
-			}
-			//update chess board
-			DressBoard(Chessboard);	
-		return Placerow;
-	    }
-	    public static int InputMode(int rounds,char[][] Chessboard,char ChessboardLine)
-	    {  
-	    	int i=rounds%2+1;//i==1:play1; i==2:play2
-	    	int modeAscii=0;
-		    System.out.printf("Player %d Select Column > ",i);
-		    String mode=stdin.nextLine();//B,T or 1-7 
-		   if(mode.length()>1)
-		   {
-			   System.out.printf("The column you enter is not right,please enter again\n");
-			   modeAscii=InputMode(rounds,Chessboard,ChessboardLine);//If it is not B,T,or 1-7, input again
-		   }
-		   else
-		   {   
-			   char modechar=mode.charAt(0);
-			   if(modechar==66||modechar==84||(modechar>=49&&modechar<=55))
-		    {
-			  modeAscii=Integer.valueOf(modechar); 
-			
-		    }
-		   else
-		   {
-		    System.out.printf("The column you enter is not right,please enter again\n");
-		    modeAscii=InputMode(rounds,Chessboard,ChessboardLine);//If it is not B,T,or 1-7, input again
-		    }
-		   }
-		    return modeAscii;
-	    }
 
-	    public static void Initchessboard(char[][] Chessboard,char ChessboardLine)
+			    
+			}
+			}
+			  
+			    
+
+	public static void Initchessboard(char[][] Chessboard,char ChessboardLine)
 	    {
 	    	for(int i = 0;i < Chessboard.length;i++)
 	    {
@@ -133,8 +118,18 @@ public class chess {
 	    }
 	    }	
 	    }
-	    public static void DressBoard(char[][] Chessboard)
+	public static void DressBoard(int totalChess,char[][] Chessboard,char ChessboardLine,int[] roundsOfTimeBomb,int rounds,int rowOfTimeBomb,int columnOfTimeBomb)
+	                            
 	    {
+			if(roundsOfTimeBomb[rounds%2]==rounds) {
+				totalChess=TimeBomb(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rowOfTimeBomb,columnOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
+			}
+			
+			else if(roundsOfTimeBomb[(rounds+1)%2]==rounds)
+			{
+				totalChess=TimeBomb(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rowOfTimeBomb,columnOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
+			}
+			
 	       for(int i=1;i<=Chessboard[0].length;i++)
 	      {
 	        System.out.printf("%d ",i);
@@ -151,8 +146,74 @@ public class chess {
 	    			System.out.printf("\n");
 	    						
 	    	}
+			}
+	    
+	public static int InputMode(int rounds,char[][] Chessboard,char ChessboardLine)
+    {  
+    	int i=rounds%2+1;//i==1:play1; i==2:play2
+    	int modeAscii=0;
+	    System.out.printf("Player %d Select Column > ",i);
+	    String mode=stdin.nextLine();//B,T or 1-7 
+	   if(mode.length()>1)
+	   {
+		   System.out.printf("The column you enter is not right,please enter again\n");
+		   modeAscii=InputMode(rounds,Chessboard,ChessboardLine);//If it is not B,T,or 1-7, input again
+	   }
+	   else
+	   {   
+		   char modechar=mode.charAt(0);
+		   if(modechar==66||modechar==84||(modechar>=49&&modechar<=55))
+	    {
+		  modeAscii=Integer.valueOf(modechar); 
+		
 	    }
+	   else
+	   {
+	    System.out.printf("The column you enter is not right,please enter again\n");
+	    modeAscii=InputMode(rounds,Chessboard,ChessboardLine);//If it is not B,T,or 1-7, input again
+	    }
+	   }
+	    return modeAscii;
+    }
+	public static int Place(int totalChess,int column,int rounds,char[][] Chessboard,char ChessboardLine,char[] chesspiece,int [] roundsOfTimeBomb,int rowOfTimeBomb,int columnOfTimeBomb)
 
+    {
+    	int Placerow=0;
+	//If ifCanPlace is true,it means It means that pieces can be placed
+	//Judge which grid to put chess on. 
+		for(int row=0;row<6;row++)//row
+		{
+		  if(Chessboard[row][column]==ChessboardLine)
+			{
+				Chessboard[row][column]=chesspiece[rounds%2];
+				Placerow=row;
+				break;
+			}
+				continue;
+		}
+		//update chess board
+		DressBoard(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
+	return Placerow;
+    }
+	public static boolean IfCanPlacePiece(int column,char[][] Chessboard,char ChessboardLine)
+	 {       
+		boolean ifCanPlace =false;
+		int row=0;
+		for(row=0;row<6;row++)//row
+	{
+		if(Chessboard[row][column]==ChessboardLine)
+		{
+			ifCanPlace=true;
+			break;
+		}		
+	}
+		if(row==Chessboard.length) 
+	{   	ifCanPlace= false;
+			System.out.print("The column of you enter is full,please enter again\n");
+			
+	}
+	    return ifCanPlace;
+	}
 	public static boolean judgeWinner(char[][] Chessboard,char CurrentChess,int row,int column)
 	 {
 		boolean ifWin=false;
@@ -173,27 +234,53 @@ public class chess {
 		  return 0;
 		  return SearchChessNumber(Chessboard, CurrentChess,r,c, i, j)+1;
 	}
-	public static boolean IfCanPlacePiece(int column,char[][] Chessboard,char ChessboardLine)
-	 {       
-		boolean ifCanPlace =false;
-		int row=0;
-		for(row=0;row<6;row++)//row
-	{
-		if(Chessboard[row][column]==ChessboardLine)
+	public static int TimeBomb(int totalChess,char[][] Chessboard,char ChessboardLine,int[] roundsOfTimeBomb,int r,int c,int rounds,int rowOfTimeBomb,int columnOfTimeBomb)
+	{                         
+		if((r+1)>0&&(r+1)<7&&c>0&&c<7&&Chessboard[r+1][c]!=ChessboardLine)
 		{
-			ifCanPlace=true;
-			break;
-		}		
-	}
-		if(row==Chessboard.length) 
-	{   	ifCanPlace= false;
-			System.out.print("The column of you enter is full,please enter again\n");
-			
-	}
-	    return ifCanPlace;
-	}
-
-	}
+			Chessboard[r+1][c]=ChessboardLine;
+			totalChess--;
+		}
+		if((r-1)>0&&(r-1)<7&&c>0&&c<7&&Chessboard[r-1][c]!=ChessboardLine)
+		{
+			Chessboard[r-1][c]=ChessboardLine;
+			totalChess--;
+		}
+		if(r>0&&r<7&&(c+1)>0&&(c+1)<7&&Chessboard[r][c+1]!=ChessboardLine)
+		{
+			Chessboard[r][c+1]=ChessboardLine;
+			totalChess--;
+		}
+		if(r>0&&r<7&&(c-1)>0&&(c-1)<7&&Chessboard[r][c-1]!=ChessboardLine)
+		{
+			Chessboard[r][c-1]=ChessboardLine;
+			totalChess--;
+		}
+		if((r-1)>0&&(r-1)<7&&(c+1)>0&&(c+1)<7&&Chessboard[r-1][c+1]!=ChessboardLine)
+		{
+			Chessboard[r-1][c+1]=ChessboardLine;
+			totalChess--;
+		}
+		if((r+1)>0&&(r+1)<7&&(c-1)>0&&(c-1)<7&&Chessboard[r+1][c-1]!=ChessboardLine)
+		{
+			Chessboard[r+1][c-1]=ChessboardLine;
+			totalChess--;
+		}
+		if((r+1)>0&&(r+1)<7&&(c+1)>0&&(c+1)<7&&Chessboard[r+1][c+1]!=ChessboardLine)
+		{
+			Chessboard[r+1][c+1]=ChessboardLine;
+			totalChess--;
+		}
+		if((r-1)>0&&(r-1)<7&&(c-1)>0&&(c-1)<7&&Chessboard[r-1][c-1]!=ChessboardLine)
+		{
+			Chessboard[r-1][c-1]=ChessboardLine;
+			totalChess--;
+		}
+		DressBoard(totalChess,Chessboard,ChessboardLine,roundsOfTimeBomb,rounds,rowOfTimeBomb,columnOfTimeBomb);
+		return totalChess;
+	
+}
+}
 
 
 
